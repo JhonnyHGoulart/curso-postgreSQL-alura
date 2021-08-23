9bf97f11d501a0f921a760276926a8c6d097a88d
Jhonny Hilário Goulart <jhonny.goulart@sc.senai.br>
2021-08-23 15:22:05 -0300

    Realizando um upgrade no id da tabela alunos, que é utilizado como foreign key na tabela aluno_curso

---
+++ scripts-curso/update-cascade.sql
@@ -0,0 +1,88 @@
+create table alunos(
+	id serial primary key,
+	nome varchar(255) not null
+);
+
+insert into alunos(nome) values('Jhonny');
+insert into alunos(nome) values('Diogo');
+insert into alunos(nome) values('Nico');
+insert into alunos(nome) values('Goku');
+insert into alunos(nome) values('Vinícius');
+
+create table cursos(
+	id serial primary key,
+	nome varchar(255) not null
+);
+
+insert into cursos(nome) values('postgreSQL');
+insert into cursos(nome) values('Java OO');
+insert into cursos(nome) values('Docker');
+
+select alunos.nome as "Aluno",
+	   cursos.nome as "Cursando"
+	from alunos
+	join aluno_curso on aluno_curso.aluno_id = alunos.id
+	join cursos 	 on cursos.id 			 = aluno_curso.curso_id;
+
+select alunos.nome as "Aluno",
+		cursos.nome as "Cursando"
+	    from alunos 
+	    left join aluno_curso on aluno_curso.aluno_id = alunos.id 
+	    left join cursos	  on cursos.id            = aluno_curso.curso_id;
+
+select alunos.nome as "Aluno",
+	   cursos.nome as "Cursando"
+	   from alunos
+	   right join aluno_curso on aluno_curso.aluno_id = alunos.id 
+	   right join cursos      on cursos.id            = aluno_curso.curso_id;
+
+select alunos.nome as "Aluno",
+	   cursos.nome as "Cursando"
+	   from alunos
+	   full join aluno_curso on aluno_curso.aluno_id = alunos.id 
+	   full join cursos      on cursos.id            = aluno_curso.curso_id;
+
+select alunos.nome as "Aluno",
+	   cursos.nome as "Cursando"
+	   from alunos
+	   cross join cursos;
+
+create table aluno_curso(
+	aluno_id integer,
+	curso_id integer,
+	primary key(aluno_id, curso_id),
+
+	foreign key (aluno_id) references alunos (id)
+	on delete cascade
+	on update cascade,
+
+	foreign key (curso_id) references cursos (id)
+	on delete cascade
+	on update cascade
+);
+
+insert into aluno_curso(aluno_id, curso_id) values(1,1);
+insert into aluno_curso(aluno_id, curso_id) values(2,1);
+insert into aluno_curso(aluno_id, curso_id) values(2,2);
+insert into aluno_curso(aluno_id, curso_id) values(3,2);
+insert into aluno_curso(aluno_id, curso_id) values(1,3);
+
+select * from cursos;
+select * from alunos;
+select * from aluno_curso;
+
+select alunos.id as "Id do aluno",
+	   cursos.id as "Id do curso",
+	   alunos.nome as "Aluno",
+	   cursos.nome as "Cursando"
+	   from alunos
+	   join aluno_curso on aluno_curso.aluno_id = alunos.id 
+	   join cursos      on cursos.id            = aluno_curso.curso_id;
+
+delete from alunos where id = 1;
+
+update alunos 	
+	set id = 10
+	where id = 1;
+
+; 
